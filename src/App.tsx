067d06382affee3fc8ad164900f350d7f47c56eb
Pedro Oliveira <phr.oliveira08@gmail.com>
2021-06-22 22:44:36 -0300

    add new pages and feature to provide login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from '../src/pages/Home'
+import { NewRoom } from './pages/NewRoom'
+
+import { AuthContextProvider } from './contexts/AuthContexts'
+
+
 function App() {
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   )
 }
 
